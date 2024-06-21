22342eb8a53f081da605c3cc31cb15899e67c28f
Anand-Prasad-14 <adrock4321anandgp@gmail.com>
2024-06-21 16:42:55 +0530

    sender views created

---
+++ AngleWish/sender/execute.py
@@ -0,0 +1,7 @@
+from .views import Task
+from apscheduler.schedulers.background import BackgroundScheduler
+
+scheduler = BackgroundScheduler()
+
+scheduler.add_job(Task, 'interval', seconds=10)
+scheduler.start()
